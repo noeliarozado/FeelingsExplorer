393a87cfa33f65ba43faa79f98e49ec2f281bbd0
Noelia Rozado <noelia.rozado@gmail.com>
2025-02-22 13:44:21 +0100

    Create Parents Guide Activity

--- app/src/main/java/com/example/feelingsexplorer/ParentsGuideActivity.kt
+++ app/src/main/java/com/example/feelingsexplorer/ParentsGuideActivity.kt
@@ -1,20 +1,21 @@
 package com.example.feelingsexplorer
 
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class ParentsGuideActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_parents_guide)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val btnBack = findViewById<Button>(R.id.btnBack)
+        btnBack.setOnClickListener {
+            finish()
         }
     }
 }
