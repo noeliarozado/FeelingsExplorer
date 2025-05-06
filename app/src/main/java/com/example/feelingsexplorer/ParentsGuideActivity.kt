955b5be825e1aab6f25e91c90f624b03edde102e
Noelia Rozado <noelia.rozado@gmail.com>
2025-05-06 14:58:30 +0200

    Improved Parents Guide

--- app/src/main/java/com/example/feelingsexplorer/ParentsGuideActivity.kt
+++ app/src/main/java/com/example/feelingsexplorer/ParentsGuideActivity.kt
@@ -2,10 +2,7 @@ package com.example.feelingsexplorer
 
 import android.os.Bundle
 import android.widget.Button
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class ParentsGuideActivity : AppCompatActivity() {
 
